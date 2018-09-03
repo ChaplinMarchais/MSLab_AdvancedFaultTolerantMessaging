04a88be04b235962f47af48736b173c5d5650feb
CMarchais <chaplin.m@impetusglobal.tech>
2018-09-03 07:08:30 -0700

    Initial commit at start of lab3, currently just fresh dotnet new console applications for both the Client and Server projects.

---
+++ service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
